1dcc827f902271e486eb83d56153e00ce14a1165
Angel Lopez Manriquez <sigma271@outlook.com>
2019-03-18 13:36:55 -0600

    Add files via upload

--- Foro/Db/foro.sql
+++ Foro/Db/foro.sql
@@ -27,7 +27,6 @@ CREATE TABLE user_post (
 -- procedures =================================================================
 
 DELIMITER &
-
 CREATE PROCEDURE get_list_post() 
 BEGIN
 
@@ -37,8 +36,25 @@ BEGIN
 	ORDER BY 1, 2, 3;
 
 END &
-
 DELIMITER ; -- end definition =================================================
+
+
+DELIMITER &
+CREATE PROCEDURE find_by_keyword(IN p_key VARCHAR(64)) 
+BEGIN
+
+	--SELECT 	p.title, p.description, p.img_url, u.nick, u.post_date, u.category
+	SELECT 	p.id, p.title, u.post_date 
+	FROM post p, user_post u 
+	WHERE p.id = u.id_post
+		AND ( p.title LIKE CONCAT("%", p_key, "%")
+			OR u.post_date LIKE CONCAT("%", p_key, "%") )
+	ORDER BY p.id, p.title;
+
+END &
+DELIMITER ; -- end definition =================================================
+
+
 
 show tables;
 desc post;
