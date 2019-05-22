29505787f8f5206bb316aa86cd693db0230b5003
ang3lino <sigma271@outlook.com>
2019-05-21 22:09:35 -0500

    Connection closed ?

--- homeworks/hanged/client.js
+++ homeworks/hanged/client.js
@@ -10,7 +10,7 @@ const readline = require('readline').createInterface({
 
 const client = new net.Socket();
 
-client.connect(PORT, HOST, function() {
+client.connect(PORT, HOST, function () {
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
 
     // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
@@ -20,30 +20,38 @@ client.connect(PORT, HOST, function() {
     })
 });
 
-client.on('data', function(data) { // response obtained from server
-    play(data);
+client.on('data', function (data) { // response obtained from server, type object
+    play(data.toString());
     client.destroy();
 });
 
 // Add a 'close' event handler for the client socket
-client.on('close', function() {
+client.on('close', function () {
     console.log('Connection closed');
 });
+
+zip = rows => rows[0].map((_, c) => rows.map(row => row[c]))
+
+String.prototype.replaceAt = function (index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
 
 function play(word) {
     let lives = 3, indexes = initialHelp(word), question = '_'.repeat(word.length)
     for (let i = 0; i < lives; ++i) {
-        indexes.forEach(j => question.charAt(j) = word.charAt(j)) 
-        readline.question(`${question}`, input => { 
-            for (let j = 0; j < input.length; ++j) if (input.charAt(j) === word.charAt(j))
-            [...input].entries.filter(t => t[0] === )
+        indexes.forEach(j => question = question.replaceAt(j, word.charAt(j)))
+        readline.question(`Type: ${question}`, oinput => {
+            input = oinput.slice(0, word.length)
+            zip(input, word).filter(t => t[0] === t[1]).forEach(t => indexes.add(t[0]))
         })
+        if (indexes.length === word.length) return true
     }
+    return false
 }
 
 // inclusive range
 function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function initialHelp(word) {
@@ -53,5 +61,6 @@ function initialHelp(word) {
     while (indexes.size < helps) {
         indexes.add(randomInt(0, b))
     }
+    console.log(indexes)
     return indexes
 }
