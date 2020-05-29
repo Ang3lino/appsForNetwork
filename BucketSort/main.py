251d9ace39c2ee36a65297d15ce56995cd3fd394
ang3lino <sigma271@outlook.com>
2020-05-29 03:39:28 -0500

    it works when list.len = 5000 ?

--- BucketSort/main.py
+++ BucketSort/main.py
@@ -6,37 +6,77 @@ import random
 import functools
 import sys
 import reliable
+import time 
+
 
 from struct import pack, unpack   
 
 
-msg_len = 4096
+def server(HOST, PORT):
+    s_req = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create a TCP Server
+    s_req.bind((HOST, PORT))
+    s_req.listen(1)
+    conn, addr = s_req.accept()
+    s_req.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    print(f'Server {HOST}:{PORT} waiting for a client')
+    object_ = pickle.loads(reliable.receive(conn))
+    print(f'Server {HOST}:{PORT} got a client ({addr})')
+    object_.sort()
+    print(object_)
+
+    reliable.send(conn, object_)
+    conn.close()
+
+def argmin(values):
+    return min(range(len(values)), key=values.__getitem__)
+
+def merge(src):
+    result = []
+    while src:
+        first = list(map(lambda arr: arr[0], src))  # get the first element from each list
+        i_min = argmin(first)
+        arr = src[i_min]  # get the reference of the list
+        result.append(arr[0])
+        arr.pop(0)
+        if not arr:
+            src.pop(i_min)
+    return result
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
-    
-def send_reliable(sock, data):
-    data_str = pickle.dumps(data)
-    data_len = pickle.dumps(len(data_str))
-    sock.send(data_len)
-    sock.sendall(data_str)
 
-def client(HOST, PORT, data):
+def client(HOST, PORT, arr):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create TCP Socket
     s.connect((HOST, PORT))
-    reliable.send(s, data)
-    result = pickle.loads(reliable.receive(s))
-    print(result)
+    reliable.send(s, arr)
+    res = pickle.loads(reliable.receive(s))
+    print(res)
+    # result[i] = res
     s.close()
 
 
-
+n_threads = 1
+# result_arr = [None] * n_threads
+rand_arr = [random.randint(0, 1000) for _ in range(5_000)]
+# partition = list(chunks(rand_arr, int(len(rand_arr) / n_threads)))  # !!
 HOST = 'localhost'
 PORT = 50007
-data = [random.randint(0, 10_000) for _ in range(5_000)]
-client(HOST, PORT, data)
 
+# servers = [threading.Thread(target=server, args=(HOST, PORT + i)) for i in range(n_threads)]
+# for t in servers:
+#     t.start()
+
+# clients = [threading.Thread(target=client, args=(HOST, PORT + i, partition[i], result_arr, i)) 
+#         for i in range(n_threads)]
+# for t in clients:
+#     t.start()
+# for t in clients:
+#     t.join()
+# print(partition)
+client(HOST, PORT, rand_arr)
+# print(result_arr)
+# print(merge(result_arr))
 
--- BucketSort/server.py
+++ BucketSort/server.py
@@ -1,5 +1,3 @@
-
-
 import socket
 import pickle
 import threading
@@ -9,27 +7,44 @@ import sys
 import reliable
 
 
-def receive_reliable(conn):
-    buff = conn.recv(8)  # expect an int, IMPORTANT to recv eight
-    length = pickle.loads(buff)  # how many bytes we got
-    bytes_ = b''
-    while len(bytes_) < length:
-        to_read = length - len(bytes_)
-        bytes_ += conn.recv(4096 if to_read > 4096 else to_read)
-    return bytes_
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def argmin(values):
+    return min(range(len(values)), key=values.__getitem__)
+
+def merge(src):
+    result = []
+    while src:
+        first = list(map(lambda arr: arr[0], src))  # get the first element from each list
+        i_min = argmin(first)
+        arr = src[i_min]  # get the reference of the list
+        result.append(arr[0])
+        arr.pop(0)
+        if not arr:
+            src.pop(i_min)
+    return result
 
 def server(HOST, PORT):
-    s_req = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create a TCP Server
-    s_req.bind((HOST, PORT))
-    s_req.listen(1)
-    conn, addr = s_req.accept()
-    s_req.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create a TCP Server
+    s.bind((HOST, PORT))
+    s.listen(1)
+    print('esperando accept')
+    conn, addr = s.accept()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print('esperando recv')
     object_ = pickle.loads(reliable.receive(conn))
     object_.sort()
     print(object_)
-
     reliable.send(conn, object_)
     conn.close()
 
+
 server('localhost', 50007)
+# arr = [random.randint(0, 10_000) for _ in range(100)]
+# arr = list(chunks(arr, 10))
+# for a in arr:
+#     a.sort()
+# print(merge(arr))
